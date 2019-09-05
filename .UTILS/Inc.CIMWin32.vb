930bdaf0dcddc91cbd5093f4da9612aedfbced76
denebgalactic <denebgalactic@users.noreply.github.com>
2019-09-05 07:38:46 +0300

    ?

---
+++ .UTILS/Inc.CIMWin32.vb
@@ -0,0 +1,112 @@
+﻿Option Strict Off : Option Explicit On
+
+#Region " Imports "
+
+Imports System.Runtime.InteropServices _
+      , System.Net.NetworkInformation _
+      , System.Security.Principal _
+      , System.Management _
+      , System.Security
+
+#End Region
+
+Namespace Emicrox
+
+    <SuppressUnmanagedCodeSecurity()>
+    Public NotInheritable Class CIMWin32
+
+#Region " .ctor "
+
+        Public Shared INTERFACES As Dictionary(Of String, ADAPTERPROPERTY) _
+                              = New Dictionary(Of String, ADAPTERPROPERTY)
+
+        <StructLayout(LayoutKind.Sequential, CharSet:=CharSet.Ansi, Pack:=1)>
+        Public Structure ADAPTERPROPERTY
+            Public [T1] As NetworkInterface _
+                 , [T2] As ManagementObject _
+                 , [T3] As ManagementObject
+        End Structure
+
+#End Region
+
+        Shared Sub Processing()
+            '
+            INTERFACES = New Dictionary(Of String, ADAPTERPROPERTY)
+            For Each adapter As NetworkInterface In NetworkInterface.GetAllNetworkInterfaces()
+                Dim T As ADAPTERPROPERTY = New ADAPTERPROPERTY
+                If Not (INTERFACES.TryGetValue(adapter.Id, T)) Then
+                    T.T1 = adapter
+                    INTERFACES.Add(adapter.Description, T)
+                End If
+            Next
+            '
+            For Each [T] As ManagementObject In New ManagementClass("Win32_NetworkAdapterConfiguration").GetInstances()
+                With [T]
+                    Dim IROPERTY As ADAPTERPROPERTY _
+                                  = New ADAPTERPROPERTY
+                    If Not (INTERFACES.TryGetValue(.GetPropertyValue("Description"), IROPERTY)) Then
+                    Else
+                        IROPERTY.T2 = [T]
+                        INTERFACES(.GetPropertyValue("Description")) = IROPERTY
+                    End If
+                End With
+            Next
+            '
+            For Each [T] As ManagementObject In New ManagementClass("Win32_NetworkAdapter").GetInstances()
+                With [T]
+                    Dim IROPERTY As ADAPTERPROPERTY _
+                                  = New ADAPTERPROPERTY
+                    If Not (INTERFACES.TryGetValue(.GetPropertyValue("Description"), IROPERTY)) Then
+                    Else
+                        IROPERTY.T3 = [T]
+                        INTERFACES(.GetPropertyValue("Description")) = IROPERTY
+                    End If
+                End With
+            Next
+            '
+        End Sub
+
+        Public Shared Sub SetValue(MethodName As String, ParameterName As String, value As Integer)
+            '
+            With New ManagementClass("Win32_NetworkAdapterConfiguration")
+                Dim [T1] As ManagementBaseObject = .GetMethodParameters(MethodName) _
+                                  , [T2] As ManagementBaseObject
+                [T1](ParameterName) = UInt32.Parse(value)
+                [T2] = .InvokeMethod(MethodName, [T1], Nothing)
+            End With
+            '
+        End Sub
+
+        Public Shared Function Disabled(ByVal ID As String) As Boolean
+            '
+            Dim IROPERTY As ADAPTERPROPERTY _
+                      = New ADAPTERPROPERTY
+            If INTERFACES.TryGetValue(ID, IROPERTY) Then
+                If Not (IsNothing(IROPERTY.T2)) Then
+                    Dim [T1] As ManagementBaseObject = IROPERTY.T3.GetMethodParameters("Disable")
+                    Dim [T2] As ManagementBaseObject _
+                            = IROPERTY.T3.InvokeMethod("Disable", [T1], Nothing)
+                End If
+            End If
+            Return True
+            '
+        End Function
+
+        Public Shared Function Enabled(ByVal ID As String) As Boolean
+            '
+            Dim IROPERTY As ADAPTERPROPERTY _
+                      = New ADAPTERPROPERTY
+            If INTERFACES.TryGetValue(ID, IROPERTY) Then
+                If Not (IsNothing(IROPERTY.T2)) Then
+                    Dim [T1] As ManagementBaseObject = IROPERTY.T3.GetMethodParameters("Enable")
+                    Dim [T2] As ManagementBaseObject _
+                                = IROPERTY.T3.InvokeMethod("Enable", [T1], Nothing)
+                End If
+            End If
+            Return True
+            '
+        End Function
+
+    End Class
+
+End Namespace
---
+++ Main.vb
@@ -0,0 +1,57 @@
+﻿#Region " Imports "
+
+Imports System.ComponentModel _
+      , System.Net.NetworkInformation _
+      , System.Security.Principal _
+      , System.Drawing.Design _
+      , System.Reflection _
+      , System.Threading _
+      , System.Management _
+      , Microsoft.Win32 _
+      , System.Text _
+      , System.IO
+
+#End Region
+
+Public Class Main
+
+    Sub Main()
+        '
+        Emicrox.CIMWin32.Disabled(NetworkInterface.GetAllNetworkInterfaces()(0).Id)
+        '
+        'Win32_NetworkAdapterConfiguration:
+        Emicrox.CIMWin32.SetValue("SetMTU", "MTU", 1500)
+        Emicrox.CIMWin32.SetValue("SetKeepAliveInterval", "KeepAliveInterval", 1000)
+        Emicrox.CIMWin32.SetValue("SetTcpipNetbios", "TcpipNetbiosOptions", 0)
+        Emicrox.CIMWin32.SetValue("SetIGMPLevel", "IGMPLevel", 2)
+        Emicrox.CIMWin32.SetValue("SetTcpWindowSize", "TcpWindowSize", 16384)
+        Emicrox.CIMWin32.SetValue("SetNumForwardPackets", "NumForwardPackets", 128)
+        Emicrox.CIMWin32.SetValue("EnableDHCP", "DHCPEnabled", True)
+        Emicrox.CIMWin32.SetValue("SetPMTUBHDetect", "PMTUBHDetectEnabled", True)
+        '......
+        '...
+        '
+        'registry
+        Registry.SetValue(String.Concat("HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\Interfaces\" _
+                                        , NetworkInterface.GetAllNetworkInterfaces()(0).Id, "\"), "TcpInitialRtt", 3, RegistryValueKind.DWord)
+        Registry.SetValue("HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters\", "DontPingGateway" _
+                                        , True, RegistryValueKind.DWord)
+        '......
+        '...
+        '
+        'WiFi:
+        Emicrox.Wlanapi_H.SetValue(Emicrox.Wlanapi_H.WlanIntfOpcode.AutoconfEnabled, True)
+        Emicrox.Wlanapi_H.SetAutoConfValue(Emicrox.Wlanapi_H.WlanAutoConfOpcode.ShowDeniedNetworks, True)
+        Emicrox.Wlanapi_H.SetAutoConfValue(Emicrox.Wlanapi_H.WlanAutoConfOpcode.AllowExplicitCredentials, True)
+        Emicrox.Wlanapi_H.SetAutoConfValue(Emicrox.Wlanapi_H.WlanAutoConfOpcode.AllowVirtualStationExtensibility, True)
+        Emicrox.Wlanapi_H.SetAutoConfValue(Emicrox.Wlanapi_H.WlanAutoConfOpcode.BlockPeriod, 5000)
+        '......
+        '...
+        '
+        Emicrox.CIMWin32.Enabled(NetworkInterface.GetAllNetworkInterfaces()(0).Id)
+        '
+        Console.ReadLine()
+        '
+    End Sub
+
+End Class
